2599f2b7406aaaeb0a34a46caa2891b03654653d
liuh <counull@163.com>
2022-09-16 09:53:11 +0800

    first commit

---
+++ Assets/CustomRP/Runtime/CustomRenderPiplineAsset.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPiplineAsset : RenderPipelineAsset {
+    protected override RenderPipeline CreatePipeline() {
+        return null;
+    }
+}
