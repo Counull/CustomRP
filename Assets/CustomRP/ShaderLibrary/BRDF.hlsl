34c0528b7d3a65661a96c75d325610ae231aa0ee
liuh <counull@163.com>
2022-09-21 22:23:44 +0800

    BRDF transparency

--- Assets/CustomRP/ShaderLibrary/BRDF.hlsl
+++ Assets/CustomRP/ShaderLibrary/BRDF.hlsl
@@ -17,11 +17,15 @@ float OneMinusReflectivity(float metallic)
     return range - metallic * range;
 }
 
-BRDF GetBRDF(Surface surface)
+BRDF GetBRDF(Surface surface, bool applyAlphaToDiffuse = false)
 {
     BRDF brdf;
     float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
     brdf.diffuse = surface.color * oneMinusReflectivity;
+    if (applyAlphaToDiffuse)
+    {
+        brdf.diffuse *= surface.alpha; // premultiplied alpha blending
+    }
     brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
     float perceptualRoughness =
         PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
@@ -31,7 +35,6 @@ BRDF GetBRDF(Surface surface)
 
 float SpecularStrength(Surface surface, BRDF brdf, Light light) //总感觉这里和half Lambert很像
 {
-    
     float3 h = SafeNormalize(light.direction + surface.viewDirection);
     float nh2 = Square(saturate(dot(surface.normal, h)));
     float lh2 = Square(saturate(dot(light.direction, h)));
@@ -39,11 +42,11 @@ float SpecularStrength(Surface surface, BRDF brdf, Light light) //总感觉这�
     float d2 = Square(nh2 * (r2 - 1.0) + 1.00001);
     float normalization = brdf.roughness * 4.0 + 2.0;
     return r2 / (d2 * max(0.1, lh2) * normalization);
-    
 }
-float3 DirectBRDF (Surface surface, BRDF brdf, Light light) {
-    return SpecularStrength(surface, brdf, light) * brdf.specular + brdf.diffuse;
 
+float3 DirectBRDF(Surface surface, BRDF brdf, Light light)
+{
+    return SpecularStrength(surface, brdf, light) * brdf.specular + brdf.diffuse;
 }
 
 
--- Assets/CustomRP/Shaders/Lit.shader
+++ Assets/CustomRP/Shaders/Lit.shader
@@ -7,6 +7,7 @@ Shader "Custom RP/Lit"
         _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
         _Metallic ("Metallic", Range(0, 1)) = 0
         _Smoothness ("Smoothness", Range(0, 1)) = 0.5
+        [Toggle(_PREMULTIPLY_ALPHA)] _PremulAlpha ("Premultiply Alpha", Float) = 0
         [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 0
         [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1
         [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend ("Dst Blend", Float) = 0
@@ -27,6 +28,7 @@ Shader "Custom RP/Lit"
             ZWrite [_ZWrite]
             HLSLPROGRAM
             #pragma target 3.5
+            #pragma shader_feature _PREMULTIPLY_ALPHA
             #pragma shader_feature _CLIPPING
             #pragma multi_compile_instancing
             #pragma vertex LitPassVertex
