f8c20dfb2e6b7ee48b7112f6b6e244fa23f3e87d
liuh <counull@163.com>
2022-10-05 16:12:59 +0800

    Sampling the Light Map

--- Assets/CustomRP/ShaderLibrary/GI.hlsl
+++ Assets/CustomRP/ShaderLibrary/GI.hlsl
@@ -1,5 +1,6 @@
 #ifndef CUSTOM_GI_INCLUDED
 #define CUSTOM_GI_INCLUDED
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/EntityLighting.hlsl"
 
 #if defined(LIGHTMAP_ON)
 #define GI_ATTRIBUTE_DATA float2 lightMapUV : TEXCOORD1;
@@ -13,15 +14,38 @@
 #define TRANSFER_GI_DATA(input, output)
 #define GI_FRAGMENT_DATA(input) 0.0
 #endif
+
+TEXTURE2D(unity_Lightmap);
+SAMPLER(samplerunity_Lightmap);
+
 struct GI
 {
     float3 diffuse;
 };
 
+
+float3 SampleLightMap(float2 lightMapUV)
+{
+    #if defined(LIGHTMAP_ON)
+    return SampleSingleLightmap(TEXTURE2D_ARGS(unity_Lightmap, samplerunity_Lightmap),
+                            lightMapUV,
+                            float4(1.0, 1.0, 0.0, 0.0),
+    #if defined(UNITY_LIGHTMAP_FULL_HDR)
+                            false,
+    #else
+                            true,
+    #endif
+                            float4(LIGHTMAP_HDR_MULTIPLIER, LIGHTMAP_HDR_EXPONENT, 0.0, 0.0)
+       );
+    #else
+    return 0.0;
+    #endif
+}
+
 GI GetGI(float2 lightMapUV)
 {
     GI gi;
-    gi.diffuse = float3(lightMapUV, 0.0);
+    gi.diffuse = SampleLightMap(lightMapUV);
     return gi;
 }
 
